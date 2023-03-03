ee95d82a914c06a70a9157918122cc560616a305
o0kam1 <duzhaokun2@outlook.com>
2023-03-03 14:24:25 +0800

    rounded applist icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -104,6 +104,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.8.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.wear:wear:1.2.0")
 
     //kotlinx-coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.6.4")
--- app/src/main/java/io/github/duzhaokun123/yamf/utils/AppInfoCache.kt
+++ app/src/main/java/io/github/duzhaokun123/yamf/utils/AppInfoCache.kt
@@ -2,16 +2,21 @@ package io.github.duzhaokun123.yamf.utils
 
 import android.content.pm.PackageInfo
 import android.graphics.drawable.Drawable
+import androidx.wear.widget.RoundedDrawable
 import io.github.duzhaokun123.yamf.xposed.utils.Instances
 
 object AppInfoCache {
-    val map = mutableMapOf<String, Pair<Drawable, CharSequence>>()
+    private val map = mutableMapOf<String, Pair<Drawable, CharSequence>>()
 
     fun getIconLabel(info: PackageInfo): Pair<Drawable, CharSequence> {
         val pn = info.packageName
         var r = map[pn]
         if (r == null) {
-            r = Instances.packageManager.getApplicationIcon(pn) to Instances.packageManager.getApplicationLabel(Instances.packageManager.getApplicationInfo(pn, 0))
+            r = RoundedDrawable().apply {
+                isClipEnabled = true
+                radius = 100
+                drawable = Instances.packageManager.getApplicationIcon(pn)
+            } to Instances.packageManager.getApplicationLabel(Instances.packageManager.getApplicationInfo(pn, 0))
             map[pn] = r
         }
         return r
